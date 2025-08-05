1192bb948a1b349876abfc9c540995703b312b93
manaswidivi <manaswinidivi9@gmail.com>
2025-08-05 11:34:05 +0530

    Create Dataconversion.java

---
+++ Dataconversion.java
@@ -0,0 +1,11 @@
+package example1;
+import java.util.*;
+public class Dataconversions {
+	public static void main(String args[]) {
+		Scanner n= new Scanner(System.in);
+		System.out.println("Enter a value");
+		int a= n.nextInt();
+		double d=a;
+		System.out.printf("%d %f", a,d);
+	}
+}
